ec26f218caaa96d1cadd8c9508ae525597064733
Ezza0806 <hazzahafizh734@gmail.com>
2025-08-22 19:07:26 +0900

    Commit pertama

---
+++ basic dart.dart
@@ -0,0 +1,17 @@
+//membuat untuk menampilkan pesan
+void main (){
+    print("Hello world");
+    test();
+}
+
+//basic dart pembuatan fungsi void, variabel, dan tipe data,
+void test() {
+    int umur = 25;
+    double tinggi= 170.5;
+    num berat = 60.5;
+    num nilai = 80;
+    print("Umur: ${umur} tahun");
+    print("Tinggi: ${tinggi} cm");
+    print("Berat: ${berat} kg");
+    print("Nilai: ${nilai}");
+}
