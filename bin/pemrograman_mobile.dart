053b3071a2d70edca3636ae5f41b737cc74a085f
Ezza0805 <hazzahafizh734@gmail.com>
2025-08-25 20:34:11 +0900

    first commit

---
+++ bin/pemrograman_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:pemrograman_mobile/pemrograman_mobile.dart' as pemrograman_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pemrograman_mobile.calculate()}!');
+}
---
+++ test/pemrograman_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:pemrograman_mobile/pemrograman_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
